b8686d97428991e87526c75f200d3f710ef8f747
Baccara <alexisbaccara@gmail.com>
2018-06-02 19:02:36 +0200

    Création du bot, ajout de code ( Const )

---
+++ index.js
@@ -0,0 +1,5 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const behest = require("behest");
+const botSignale = require("signale");
+const rules = require("./rules.js");
