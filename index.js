fb136294b8441d842e80a569c9d18ea3d623ef55
Baccara <alexisbaccara@gmail.com>
2018-06-02 19:07:18 +0200

    Ajout de la connexion de notre bot sur notre serveur Discord

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const behest = require("behest");
 const botSignale = require("signale");
-const rules = require("./rules.js");
+
+console.log(rules);
+
+client.on("ready", () => {
+  client.user.setPresence({ game: { name: "!cmd : Voir mes commandes" } });
+  console.log(`Connecté en tant que ${client.user.tag}!`);
+});
+
+client.login(`${token}`);
