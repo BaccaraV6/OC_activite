196374e6de63dff4e0383b1364a963f11f439b6a
Baccara <alexisbaccara@gmail.com>
2018-06-02 19:10:25 +0200

    ajout de la commande !cmd permettant d'afficher les commandes, de les afficher sur un channel et d'avoir un log des commandes utiliser sur le serveur.

--- index.js
+++ index.js
@@ -11,3 +11,16 @@ client.on("ready", () => {
 });
 
 client.login(`${token}`);
+
+client.on("message", message => {
+  if (message.content === "!cmd") {
+    message.channel
+      .send(rules.getEmbed(client))
+      .then(() => botSignale.success("Commande CMD demandée"));
+    client.channels
+      .get("id_channel")
+      .send(
+        "La commande CMD a bien été éxécuté par " + message.author.username
+      );
+  }
+});
